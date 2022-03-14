f83faedc41eabd10ecf43c5d6c606d242a07a350
liric <s-liric@bsd405.org>
2022-03-14 13:06:36 -0700

    commit

--- app/src/main/java/com/example/sevens3/MainActivity.java
+++ app/src/main/java/com/example/sevens3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sevens3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToActivityContent(View view)
+    {
+        ImageButton mainPageButton = findViewById(R.id.mainPageButton);
+
+        try {
+            Intent k = new Intent(MainActivity.this, ContentActivity.class);
+            startActivity(k);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
