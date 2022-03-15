c06c7279a4068f12a13413124cba8dd751a1c54d
liric <s-liric@bsd405.org>
2022-03-15 13:08:42 -0700

    commit

--- app/src/main/java/com/example/sevens3/ContentActivity.java
+++ app/src/main/java/com/example/sevens3/ContentActivity.java
@@ -2,7 +2,11 @@ package com.example.sevens3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ContentActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class ContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+    }
+
+    public void goToBreakActivity(View view)
+    {
+        Button breakButton = findViewById(R.id.breakButton);
+
+        try {
+            Intent k = new Intent(ContentActivity.this, ContentActivity.class);
+            startActivity(k);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
