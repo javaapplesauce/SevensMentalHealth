788e667443ce4ecf4ea8e89c6e605ce673e77dfd
liric <s-liric@bsd405.org>
2022-03-22 13:08:29 -0700

    commit

--- app/src/main/java/com/example/sevens3/BreakActivityAuto.java
+++ app/src/main/java/com/example/sevens3/BreakActivityAuto.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BreakActivityAuto extends AppCompatActivity {
+
+    private int displayedNum;
+    private int timesClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +18,31 @@ public class BreakActivityAuto extends AppCompatActivity {
         setContentView(R.layout.activity_break_auto);
     }
 
+    public void BreakActivityAuto()
+    {
+        displayedNum = 0;
+        timesClicked = 0;
+    }
+
+    // adds to the number of times the button has been clicked so that the number can be added in the future
+    public void addToClicks()
+    {
+        this.timesClicked++;
+    }
+
+
     public void goToBreakManual(View view)
     {
+
+    }
+
+    public void countUp(View view)
+    {
+        Button ClickUp = findViewById(R.id.CountsUp);
+        TextView DisplayNumber = findViewById(R.id.NumberDisplay);
+        addToClicks();
+        int numberToDisplay = (displayedNum + 7*timesClicked);
+        DisplayNumber.setText(numberToDisplay);
 
     }
 
