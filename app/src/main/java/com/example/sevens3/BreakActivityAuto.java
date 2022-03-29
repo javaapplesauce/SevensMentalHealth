1d8cf9833bc0835f8b7ce27883bc354ca66d14d5
liric <s-liric@bsd405.org>
2022-03-29 13:51:51 -0700

    commit

--- app/src/main/java/com/example/sevens3/BreakActivityAuto.java
+++ app/src/main/java/com/example/sevens3/BreakActivityAuto.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +15,21 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 public class BreakActivityAuto extends AppCompatActivity {
+
+    TextView tvTimer;
+    long startTime, timeInMilliseconds = 0;
+    Handler customHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_break_auto);
+
+        tvTimer = (TextView) findViewById(R.id.tvTimer);
     }
 
     public void goToBreakManual(View view)
@@ -33,16 +44,27 @@ public class BreakActivityAuto extends AppCompatActivity {
         }
     }
 
-    public void goBack(View view)
-    {
-        ImageButton backButton = findViewById(R.id.backButton2);
-
-        try {
-            Intent k = new Intent(BreakActivityAuto.this, ContentActivity.class);
-            startActivity(k);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+    public static String getDateFromMillis(long d) {
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return df.format(d);
     }
+
+    public void start(View v) {
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread, 0);
+    }
+
+    public void stop(View v) {
+        customHandler.removeCallbacks(updateTimerThread);
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            tvTimer.setText(getDateFromMillis(timeInMilliseconds));
+            customHandler.postDelayed(this, 1000);
+        }
+    };
 
 }
